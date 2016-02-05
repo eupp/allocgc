a64132b439b9e7d640af2933c08e36def8fe1fdd
Evgeniy Moiseenko <evg.moiseenko@mail.ru>
2016-02-05 19:55:17 +0300

    gc_new_state

---
+++ precisegc/include/libprecisegc/details/gc_new_state.h
@@ -0,0 +1,50 @@
+#ifndef DIPLOMA_GC_NEW_STATE_H
+#define DIPLOMA_GC_NEW_STATE_H
+
+#include <cstddef>
+#include <vector>
+#include <memory>
+
+#include "util.h"
+
+namespace precisegc { namespace details {
+
+class gc_new_state: public noncopyable, public nonmovable
+{
+public:
+    class stack_entry
+    {
+    public:
+        stack_entry();
+        ~stack_entry();
+
+        void set_new_state(void* new_ptr);
+    private:
+        std::vector<size_t> m_old_offsets;
+        void* m_old_current_ptr;
+        bool m_old_is_active;
+        bool m_new_state_set;
+    };
+
+    static gc_new_state& instance();
+
+    std::vector<size_t>& get_offsets();
+
+    void* get_current_pointer() const noexcept;
+    void set_current_pointer(void* ptr) noexcept;
+
+    bool is_active() const noexcept;
+    void set_active(bool active) noexcept;
+private:
+    static thread_local std::unique_ptr<gc_new_state> state_ptr;
+
+    gc_new_state();
+
+    std::vector<size_t> m_offsets;
+    void* m_current_ptr;
+    bool m_is_active;
+};
+
+}}
+
+#endif //DIPLOMA_GC_NEW_STATE_H

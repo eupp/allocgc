0ba8b7825416be4584b62860744b87225e1958cc
Evgeniy Moiseenko <evg.moiseenko@mail.ru>
2016-02-08 14:23:52 +0300

    split gc_untyped_ptr_test construct/assignment tests

---
+++ test/unit_tests/details/gc_untyped_ptr_test.cpp
@@ -0,0 +1,135 @@
+#include <gtest/gtest.h>
+
+#include <memory>
+#include <utility>
+
+#include "libprecisegc/details/gc_untyped_ptr.h"
+#include "libprecisegc/details/gc_new_stack.h"
+
+using namespace precisegc::details;
+
+TEST(gc_untyped_ptr_test, test_default_construct)
+{
+    gc_untyped_ptr ptr;
+    EXPECT_EQ(nullptr, ptr.get());
+}
+
+TEST(gc_untyped_ptr_test, test_nullptr_construct)
+{
+    gc_untyped_ptr ptr(nullptr);
+    EXPECT_EQ(nullptr, ptr.get());
+}
+
+TEST(gc_untyped_ptr_test, test_raw_ptr_construct)
+{
+    int val;
+    void* ptr = (void*) &val;
+    gc_untyped_ptr ptr1(ptr);
+    EXPECT_EQ(ptr, ptr1.get());
+}
+
+TEST(gc_untyped_ptr_test, test_copy_construct)
+{
+    int val;
+    void* ptr = (void*) &val;
+    gc_untyped_ptr ptr1(ptr);
+
+    gc_untyped_ptr ptr2(ptr1);
+    EXPECT_EQ(ptr, ptr2.get());
+}
+
+TEST(gc_untyped_ptr_test, test_move_construct)
+{
+    int val;
+    void* ptr = (void*) &val;
+    gc_untyped_ptr ptr1(ptr);
+
+    gc_untyped_ptr ptr2(std::move(ptr1));
+    EXPECT_EQ(ptr, ptr2.get());
+}
+
+TEST(gc_untyped_ptr_test, test_nullptr_assignment)
+{
+    int val;
+    void* ptr = (void*) &val;
+
+    gc_untyped_ptr ptr1(ptr);
+    ptr1 = nullptr;
+    EXPECT_EQ(nullptr, ptr1.get());
+}
+
+TEST(gc_untyped_ptr_test, test_copy_assignment)
+{
+    int val;
+    void* ptr = (void*) &val;
+
+    gc_untyped_ptr ptr1(ptr);
+    gc_untyped_ptr ptr2;
+    ptr2 = ptr1;
+    EXPECT_EQ(ptr, ptr2.get());
+}
+
+TEST(gc_untyped_ptr_test, test_move_assignment)
+{
+    int val;
+    void* ptr = (void*) &val;
+
+    gc_untyped_ptr ptr1(ptr);
+    gc_untyped_ptr ptr2;
+    ptr2 = std::move(ptr1);
+    EXPECT_EQ(ptr, ptr2.get());
+}
+
+TEST(gc_untyped_ptr_test, test_is_root_1)
+{
+    gc_untyped_ptr ptr1;
+    EXPECT_TRUE(ptr1.is_root());
+
+    gc_new_stack::activation_entry activation_entry;
+    gc_untyped_ptr ptr2;
+    EXPECT_FALSE(ptr2.is_root());
+}
+
+TEST(gc_untyped_ptr_test, test_is_root_2)
+{
+    int val;
+    void* ptr = (void*) &val;
+
+    gc_untyped_ptr ptr1(ptr);
+
+    gc_untyped_ptr ptr2;
+    gc_new_stack::activation_entry activation_entry;
+    gc_untyped_ptr ptr3;
+
+    ptr2 = ptr1;
+    EXPECT_TRUE(ptr2.is_root());
+
+    ptr3 = ptr1;
+    EXPECT_FALSE(ptr3.is_root());
+}
+
+TEST(gc_untyped_ptr_test, test_bool_conversion)
+{
+    gc_untyped_ptr ptr1;
+    EXPECT_FALSE(ptr1);
+
+    int val;
+    void* ptr = (void*) &val;
+
+    gc_untyped_ptr ptr2(ptr);
+    EXPECT_TRUE((bool) ptr2);
+}
+
+TEST(gc_untyped_ptr_test, test_swap)
+{
+    int val;
+    void* ptr = (void*) &val;
+
+    gc_untyped_ptr ptr1;
+    gc_untyped_ptr ptr2(ptr);
+
+    swap(ptr1, ptr2);
+
+    EXPECT_EQ(ptr, ptr1.get());
+    EXPECT_EQ(nullptr, ptr2.get());
+}

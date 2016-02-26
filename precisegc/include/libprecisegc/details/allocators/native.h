ade8b32724c554733139238e88779d333f002070
Evgeniy Moiseenko <evg.moiseenko@mail.ru>
2016-02-26 15:41:25 +0300

    native allocator

---
+++ precisegc/include/libprecisegc/details/allocators/native.h
@@ -0,0 +1,23 @@
+#ifndef DIPLOMA_MMAP_H
+#define DIPLOMA_MMAP_H
+
+#include <cstddef>
+#include <cstdint>
+
+namespace precisegc { namespace details { namespace allocators {
+
+typedef std::uint8_t byte;
+
+byte* memory_allocate(size_t size);
+void memory_deallocate(byte* ptr, size_t size);
+
+class native_allocator
+{
+public:
+    byte* allocate(size_t);
+    void deallocate(byte* ptr, size_t size);
+};
+
+}}}
+
+#endif //DIPLOMA_MMAP_H
---
+++ precisegc/src/details/allocators/native.cpp
@@ -0,0 +1,39 @@
+#include "allocators/native.h"
+
+#include <cstring>
+#include <sys/mman.h>
+
+#include "logging.h"
+
+namespace precisegc { namespace details { namespace allocators {
+
+byte* memory_allocate(size_t size)
+{
+    void* mem = mmap(0, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    if (mem == MAP_FAILED) {
+        logging::debug() << "mmap failed: " << strerror(errno);
+        assert(false);
+    }
+    return reinterpret_cast<byte*>(mem);
+}
+
+void memory_deallocate(byte* ptr, size_t size)
+{
+    int ret = munmap(reinterpret_cast<void*>(ptr), size);
+    if (ret == -1) {
+        logging::debug() << "munmap failed: " << strerror(errno);
+    }
+}
+
+byte* native_allocator::allocate(size_t size)
+{
+    return memory_allocate(size);
+}
+
+void native_allocator::deallocate(byte* ptr, size_t size)
+{
+    memory_deallocate(ptr, size);
+}
+
+}}}
+

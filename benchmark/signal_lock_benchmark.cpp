e8da6f22fe78817aa51c80d40e15b60e94bd1265
Evgeniy Moiseenko <evg.moiseenko@mail.ru>
2016-03-25 13:07:22 +0300

    benchmark for flag_gc_signal_lock

--- benchmark/signal_lock_benchmark.cpp
+++ benchmark/signal_lock_benchmark.cpp
@@ -1,6 +1,7 @@
 #include <nonius/nonius.h++>
 
 #include "libprecisegc/details/sigmask_signal_lock.h"
+#include "libprecisegc/details/gc_pause.h"
 
 using namespace precisegc::details;
 
@@ -8,14 +9,14 @@ NONIUS_BENCHMARK("sigmask_signal_lock", [](nonius::chronometer meter)
 {
     meter.measure([] {
         sigmask_gc_signal_lock::lock();
-        sigmask_gc_signal_lock::unlock();
+        return sigmask_gc_signal_lock::unlock();
     });
 });
 
-
-
-//NONIUS_BENCHMARK("sigmask_signal_unlock", [](nonius::chronometer meter)
-//{
-//    sigmask_gc_signal_lock::lock();
-//    meter.measure([] { return sigmask_gc_signal_lock::unlock(); });
-//});
+NONIUS_BENCHMARK("flag_signal_lock", [](nonius::chronometer meter)
+{
+    meter.measure([] {
+        flag_gc_signal_lock::lock();
+        return flag_gc_signal_lock::unlock();
+    });
+});

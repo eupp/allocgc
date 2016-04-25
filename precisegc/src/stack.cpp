3d7bcfb618500d6951b6e9c22a15500daaf7cc06
Evgeniy Moiseenko <evg.moiseenko@mail.ru>
2016-04-25 20:39:49 +0300

    unsafe scope in stack root

--- precisegc/src/stack.cpp
+++ precisegc/src/stack.cpp
@@ -6,6 +6,8 @@
 #include <sys/mman.h>
 #include <assert.h>
 
+#include "details/gc_unsafe_scope.h"
+
 //StackMap * StackMap::getInstance() {
 //	static thread_local StackMap instance;
 //	return &instance;
@@ -15,7 +17,7 @@ constexpr int PAGE_SIZE = 4096;
 constexpr int ELEM_PER_PAGE = PAGE_SIZE / sizeof(StackElement);
 
 void StackMap::register_stack_root(void * newAddr) {
-//    precisegc::details::lock_guard<mutex_type> lock(m_mutex);
+	precisegc::details::gc_unsafe_scope unsafe_scope;
 	if (free_list == nullptr) {
 		StackElement * data = (StackElement *) mmap(nullptr, PAGE_SIZE, PROT_WRITE | PROT_READ, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
 		assert(data != MAP_FAILED);
@@ -33,7 +35,7 @@ void StackMap::register_stack_root(void * newAddr) {
 }
 
 void StackMap::delete_stack_root(void * address) {
-//    precisegc::details::lock_guard<mutex_type> lock(m_mutex);
+    precisegc::details::gc_unsafe_scope unsafe_scope;
 	if (top->addr != address) {
 		// if it is so, then automatic objects dectructs
 		// not in the reverse order with their constructors

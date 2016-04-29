c321c9a51deee8c549a3183e36528cf6fce13d1a
Evgeniy Moiseenko <evg.moiseenko@mail.ru>
2016-04-29 17:36:54 +0300

    dijcstra write-barrier

--- precisegc/src/gc_mark_queue.cpp
+++ precisegc/src/gc_mark_queue.cpp
@@ -1,5 +1,7 @@
 #include "gc_mark_queue.h"
 
+#include "managed_ptr.h"
+
 #include "logging.h"
 
 namespace precisegc { namespace details {
@@ -26,6 +28,29 @@ bool gc_mark_queue::push(void* ptr)
 
 bool gc_mark_queue::pop(void*& p)
 {
+    p = nullptr;
+    void* ptr = nullptr;
+    if (m_queue.pop(ptr)) {
+        managed_cell_ptr cell_ptr(managed_ptr(reinterpret_cast<byte*>(ptr)), 0);
+        if (!cell_ptr.get_mark()) {
+            p = ptr;
+        }
+        return true;
+    }
+    return false;
+
+//    // this check will be failed only when ptr is pointed to non gc_heap memory,
+//    // that is not possible in correct program (i.e. when gc_new is used to create managed objects),
+//    // but could occur during testing.
+//    try {
+//        cell_ptr.lock_descriptor();
+//        if (!cell_ptr.get_mark()) {
+////            static gc_mark_queue& queue = gc_mark_queue::instance();
+//            queue->push(ptr);
+//        }
+//    } catch (managed_cell_ptr::unindexed_memory_exception& exc) {
+//        return;
+//    }
     return m_queue.pop(p);
 }
 

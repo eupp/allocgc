7147456d9eecd2c04f8f438eab70e7f183c46264
Evgeniy Moiseenko <evg.moiseenko@mail.ru>
2016-05-24 20:58:09 +0300

    fast lsb

--- precisegc/include/libprecisegc/details/math_util.h
+++ precisegc/include/libprecisegc/details/math_util.h
@@ -69,11 +69,35 @@ inline size_t msb(unsigned long long n)
     return lo;
 }
 
-// not a best solution
+/**
+ * implementation is taken from http://chessprogramming.wikispaces.com/BitScan
+ *
+ * bitScanForward
+ * @author Martin Läuter (1997)
+ *         Charles E. Leiserson
+ *         Harald Prokop
+ *         Keith H. Randall
+ * "Using de Bruijn Sequences to Index a 1 in a Computer Word"
+ * @param bb bitboard to scan
+ * @precondition bb != 0
+ * @return index (0..63) of least significant one bit
+ */
 inline size_t lsb(unsigned long long n)
 {
-    assert(n != 0);
-    return msb(n & -n);
+    static const size_t index64[64] = {
+            0,  1, 48,  2, 57, 49, 28,  3,
+            61, 58, 50, 42, 38, 29, 17,  4,
+            62, 55, 59, 36, 53, 51, 43, 22,
+            45, 39, 33, 30, 24, 18, 12,  5,
+            63, 47, 56, 27, 60, 41, 37, 16,
+            54, 35, 52, 21, 44, 32, 23, 11,
+            46, 26, 40, 15, 34, 20, 31, 10,
+            25, 14, 19,  9, 13,  8,  7,  6
+    };
+
+    const unsigned long long debruijn64 = 0x03f79d71b4cb0a89ULL;
+    assert (n != 0);
+    return index64[((n & -n) * debruijn64) >> 58];
 }
 
 }}

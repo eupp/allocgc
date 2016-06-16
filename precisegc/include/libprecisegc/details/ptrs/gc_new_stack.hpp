7fe5f26aca4447b5dde2e48039f293070a30f767
Evgeniy Moiseenko <evg.moiseenko@mail.ru>
2016-06-16 14:31:04 +0300

    get rid of tls-init, better shadow stack implementation

--- precisegc/include/libprecisegc/details/ptrs/gc_new_stack.hpp
+++ precisegc/include/libprecisegc/details/ptrs/gc_new_stack.hpp
@@ -15,8 +15,9 @@ namespace precisegc { namespace details { namespace ptrs {
 
 class gc_new_stack : public utils::noncopyable, public utils::nonmovable
 {
+    class stack_top;
 public:
-    typedef utils::dynarray<size_t> offsets_storage_t;
+    typedef std::vector<size_t> offsets_storage_t;
     typedef offsets_storage_t::const_iterator offsets_iterator;
     typedef boost::iterator_range<offsets_iterator> offsets_range;
 
@@ -30,14 +31,15 @@ public:
     class stack_entry
     {
     public:
-        stack_entry(void* new_ptr, size_t new_size);
+        stack_entry(void* ptr, size_t size);
         ~stack_entry();
     private:
-        void* m_old_ptr;
-        size_t m_old_size;
-        bool m_old_is_meta_requested;
-        size_t m_old_offsets_cnt;
-        offsets_storage_t m_old_offsets;
+        friend class stack_top;
+
+        void*  m_ptr;
+        size_t m_size;
+        offsets_storage_t m_offsets;
+        stack_entry* m_prev;
     };
 
     static void register_child(gc_untyped_ptr* child);
@@ -48,7 +50,33 @@ public:
     static bool is_active() noexcept;
     static bool is_meta_requsted() noexcept;
 private:
+    class stack_top
+    {
+    public:
+        constexpr stack_top()
+            : m_stack_top(nullptr)
+            , m_depth(0)
+        {}
+
+        void register_child(gc_untyped_ptr* child);
+
+        gc_new_stack::offsets_range offsets() const;
+
+        size_t depth() const noexcept;
+        bool is_active() const noexcept;
+        bool is_meta_requsted() const noexcept;
+
+        friend class gc_new_stack;
+        friend class gc_new_stack::stack_entry;
+        friend class gc_new_stack::activation_entry;
+    private:
+        gc_new_stack::stack_entry* m_stack_top;
+        size_t m_depth;
+    };
+
     static const size_t START_OFFSETS_STORAGE_SIZE = 64;
+
+    static thread_local stack_top top;
 };
 
 }}}
--- precisegc/src/details/ptrs/gc_new_stack.cpp
+++ precisegc/src/details/ptrs/gc_new_stack.cpp
@@ -5,96 +5,52 @@
 
 namespace precisegc { namespace details { namespace ptrs {
 
-class stack_top
+thread_local gc_new_stack::stack_top gc_new_stack::top{};
+
+void gc_new_stack::stack_top::register_child(gc_untyped_ptr* child)
 {
-public:
-    constexpr stack_top()
-        : m_top_ptr(nullptr)
-        , m_top_size(0)
-        , m_depth(0)
-        , m_is_meta_requested(false)
-        , m_top_offsets_cnt(0)
-    {}
-
-    void register_child(gc_untyped_ptr* child)
-    {
-        std::uintptr_t top  = reinterpret_cast<std::uintptr_t>(m_top_ptr);
-        std::uintptr_t curr = reinterpret_cast<std::uintptr_t>(child);
-        if (top <= curr && curr < top + m_top_size) {
-            push_offset(curr - top);
-        }
+    assert(m_stack_top);
+    std::uintptr_t top  = reinterpret_cast<std::uintptr_t>(m_stack_top->m_ptr);
+    std::uintptr_t curr = reinterpret_cast<std::uintptr_t>(child);
+    if (top <= curr && curr < top + m_stack_top->m_size) {
+        m_stack_top->m_offsets.push_back(curr - top);
     }
+}
 
-    gc_new_stack::offsets_range offsets() const
-    {
-        return boost::make_iterator_range(m_top_offsets.begin(), std::next(m_top_offsets.begin(), m_top_offsets_cnt));
-    }
+gc_new_stack::offsets_range gc_new_stack::stack_top::offsets() const
+{
+    return boost::make_iterator_range(m_stack_top->m_offsets.begin(),
+                                      m_stack_top->m_offsets.end());
+}
 
-    size_t depth() const noexcept
-    {
-        return m_depth;
-    }
+size_t gc_new_stack::stack_top::depth() const noexcept
+{
+    return m_depth;
+}
 
-    bool is_active() const noexcept
-    {
-        return m_depth > 0;
-    }
+bool gc_new_stack::stack_top::is_active() const noexcept
+{
+    return m_depth > 0;
+}
 
-    bool is_meta_requsted() const noexcept
-    {
-        return m_is_meta_requested;
-    }
+bool gc_new_stack::stack_top::is_meta_requsted() const noexcept
+{
+    return m_stack_top != nullptr;
+}
 
-    friend class gc_new_stack::stack_entry;
-    friend class gc_new_stack::activation_entry;
-private:
-    void push_offset(size_t offset)
-    {
-        if (m_top_offsets_cnt == m_top_offsets.size()) {
-            utils::dynarray<size_t> new_offsets(2 * m_top_offsets_cnt);
-            std::copy(m_top_offsets.begin(), m_top_offsets.end(), new_offsets.begin());
-            m_top_offsets = new_offsets;
-        }
-        m_top_offsets[m_top_offsets_cnt++] = offset;
-    }
-
-    void* m_top_ptr;
-    size_t m_top_size;
-    size_t m_depth;
-    bool m_is_meta_requested;
-    size_t m_top_offsets_cnt;
-    gc_new_stack::offsets_storage_t m_top_offsets;
-};
-
-static thread_local stack_top top{};
-
-gc_new_stack::stack_entry::stack_entry(void* new_ptr, size_t new_size)
-    : m_old_offsets(START_OFFSETS_STORAGE_SIZE)
+gc_new_stack::stack_entry::stack_entry(void* ptr, size_t size)
+    : m_ptr(ptr)
+    , m_size(size)
+    , m_prev(top.m_stack_top)
 {
     assert(top.is_active());
-
-    m_old_ptr = top.m_top_ptr;
-    top.m_top_ptr = new_ptr;
-
-    m_old_size = top.m_top_size;
-    top.m_top_size = new_size;
-
-    m_old_is_meta_requested = top.m_is_meta_requested;
-    top.m_is_meta_requested = true;
-
-    m_old_offsets_cnt = top.m_top_offsets_cnt;
-    top.m_top_offsets_cnt = 0;
-
-    m_old_offsets.swap(top.m_top_offsets);
+    top.m_stack_top = this;
+    m_offsets.reserve(START_OFFSETS_STORAGE_SIZE);
 }
 
 gc_new_stack::stack_entry::~stack_entry()
 {
-    top.m_top_ptr = m_old_ptr;
-    top.m_top_size = m_old_size;
-    top.m_is_meta_requested = m_old_is_meta_requested;
-    top.m_top_offsets_cnt = m_old_offsets_cnt;
-    top.m_top_offsets.swap(m_old_offsets);
+    top.m_stack_top = m_prev;
 }
 
 gc_new_stack::activation_entry::activation_entry()

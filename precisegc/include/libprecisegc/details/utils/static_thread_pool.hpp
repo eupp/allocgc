0bad5eeb26eb1088c655498387be3362937c32f6
Evgeniy Moiseenko <evg.moiseenko@mail.ru>
2016-07-22 18:18:26 +0300

    fix static_thread_pool

--- precisegc/include/libprecisegc/details/utils/static_thread_pool.hpp
+++ precisegc/include/libprecisegc/details/utils/static_thread_pool.hpp
@@ -24,10 +24,11 @@ public:
         , m_done(false)
         , m_tasks_cnt(0)
         , m_complete_tasks_cnt(0)
+        , m_tasks_ready(false)
     {
         auto guard = make_scope_guard([this] { stop_workers(); });
-        for (auto& thread: m_threads) {
-            thread = std::thread(&precisegc::details::utils::static_thread_pool::thread_routine, this);
+        for (size_t i = 0; i < m_threads.size(); ++i) {
+            m_threads[i] = std::thread(&precisegc::details::utils::static_thread_pool::thread_routine, this, i);
         }
         guard.commit();
     }
@@ -50,30 +51,33 @@ public:
         }
         m_tasks_cnt = m_tasks.size();
         m_complete_tasks_cnt = 0;
+        m_tasks_ready = true;
         m_tasks_ready_cond.notify_all();
 
         m_tasks_complete_cond.wait(lock, [this] { return m_complete_tasks_cnt == m_tasks_cnt; });
+        m_tasks_ready = false;
     }
 private:
     void stop_workers()
     {
+        std::lock_guard<std::mutex> lock(m_mutex);
         m_done = true;
         m_tasks_ready_cond.notify_all();
     }
 
-    void thread_routine()
+    void thread_routine(size_t thread_num)
     {
         while (true) {
             std::unique_lock<std::mutex> lock(m_mutex);
-            m_tasks_ready_cond.wait(lock, [this] { return !m_tasks.empty() || m_done; });
+            m_tasks_ready_cond.wait(lock, [this] { return (m_tasks_ready && !m_tasks.empty()) || m_done; });
             if (m_done) {
                 return;
             }
 
-            size_t tasks_per_worker = (m_tasks_cnt + m_threads.size() - 1) / m_threads.size();
-            size_t pop_cnt = std::min(tasks_per_worker, m_tasks.size());
-            std::vector<std::function<void()>> worker_tasks(m_tasks.rbegin(), std::next(m_tasks.rbegin(), pop_cnt));
-            m_tasks.resize(m_tasks.size() - pop_cnt);
+            size_t tasks_per_worker = m_tasks_cnt / m_threads.size();
+            tasks_per_worker += thread_num < (m_tasks_cnt % m_threads.size()) ? 1 : 0;
+            std::vector<std::function<void()>> worker_tasks(m_tasks.rbegin(), std::next(m_tasks.rbegin(), tasks_per_worker));
+            m_tasks.resize(m_tasks.size() - tasks_per_worker);
             lock.unlock();
 
             for (auto& task: worker_tasks) {
@@ -81,19 +85,21 @@ private:
             }
 
             lock.lock();
-            m_complete_tasks_cnt += pop_cnt;
+            m_complete_tasks_cnt += tasks_per_worker;
             m_tasks_complete_cond.notify_all();
+            m_tasks_complete_cond.wait(lock, [this] { return m_complete_tasks_cnt == m_tasks_cnt; });
         }
     }
 
     dynarray<scoped_thread> m_threads;
-    std::atomic<bool> m_done;
     std::vector<std::function<void()>> m_tasks;
     size_t m_tasks_cnt;
     size_t m_complete_tasks_cnt;
     std::mutex m_mutex;
     std::condition_variable m_tasks_ready_cond;
     std::condition_variable m_tasks_complete_cond;
+    bool m_tasks_ready;
+    bool m_done;
 };
 
 }}}

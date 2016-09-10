7f6a6bdcc98c780862dee816dbbc823d9ff1f6fa
Evgeniy Moiseenko <evg.moiseenko@mail.ru>
2016-09-10 10:36:33 +0300

    gc_heap::allocate benchmark

--- microbenchmark/allocators_benchmark.cpp
+++ microbenchmark/allocators_benchmark.cpp
@@ -4,9 +4,11 @@
 
 #include <libprecisegc/details/allocators/default_allocator.hpp>
 #include <libprecisegc/details/allocators/pool.hpp>
+#include <libprecisegc/details/gc_heap.hpp>
 
 #include "deoptimize.hpp"
 
+using namespace precisegc;
 using namespace precisegc::details;
 using namespace precisegc::details::allocators;
 
@@ -53,5 +55,13 @@ NONIUS_BENCHMARK("allocators.pool.deallocate", [](nonius::chronometer meter)
     }
     meter.measure([&allocator, &ps] (size_t i) {
         allocator.deallocate(ps[i]);
+    });
+});
+
+NONIUS_BENCHMARK("allocators.gc_heap.allocate", [](nonius::chronometer meter)
+{
+    gc_heap heap(gc_compacting::DISABLED);
+    meter.measure([&heap] () {
+        return heap.allocate(OBJ_SIZE);
     });
 });

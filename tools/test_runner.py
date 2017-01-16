0090275ad3018df45f84bbb72b7478cb27825a55
Evgeniy Moiseenko <evg.moiseenko@mail.ru>
2017-01-16 18:32:23 +0300

    script for running tests after build in CI

--- tools/test_runner.py
+++ tools/test_runner.py
@@ -1,14 +1,15 @@
 import os
 import re
+import sys
+import json
+import math
+import shutil
+import getopt
+import logging
+import tempfile
 import subprocess
 import collections
-import tempfile
-import logging
-import json
-import tabulate
-import shutil
 
-import numpy as np
 
 STATS_FULL_TIME_MEAN    = "full time mean"
 STATS_FULL_TIME_STD     = "full time std"
@@ -20,16 +21,27 @@ STATS_STW_TIME_MAX      = "stw time max"
 STATS_GC_COUNT          = "gc count"
 
 
+def mean(xs):
+    s = 0.0
+    for x in xs:
+        s += x
+    return s / len(xs)
+
+
+def std(xs):
+    return math.sqrt(mean([x**2 for x in xs]) - mean(xs)**2)
+
+
 def stat_mean(arr, default=float('NaN'), ndigits=None):
-    return round(np.mean(arr), ndigits) if len(arr) > 0 else default
+    return round(mean(arr), ndigits) if len(arr) > 0 else default
 
 
 def stat_std(arr, default=float('NaN'), ndigits=None):
-    return round(np.std(arr), ndigits) if len(arr) > 0 else default
+    return round(std(arr), ndigits) if len(arr) > 0 else default
 
 
 def stat_max(arr, default=float('NaN'), ndigits=None):
-    return round(np.max(arr), ndigits) if len(arr) > 0 else default
+    return round(max(arr), ndigits) if len(arr) > 0 else default
 
 
 def call_with_cwd(args, cwd):
@@ -45,14 +57,15 @@ def call_output(args, cwd=None, timeout=None):
     except subprocess.TimeoutExpired:
         proc.kill()
         raise
-    return (proc.returncode, out.decode("utf-8"))
+    return proc.returncode, out.decode("utf-8")
 
 
-class Builder:
+class Build:
 
-    def __init__(self, name, prj_dir):
+    def __init__(self, name, prj_dir, runnable):
         self._name = name
         self._prj_dir = prj_dir
+        self._runnable = runnable
         self._tmpdir = tempfile.TemporaryDirectory()
 
     def __enter__(self):
@@ -71,17 +84,28 @@ class Builder:
     def project_dir(self):
         return self._prj_dir
 
+    def runnable(self):
+        return self._runnable
 
-class CMakeBuilder(Builder):
+
+class CMakeBuild(Build):
 
     _tmpdir = None
 
-    def __init__(self, prj_dir, target, runnable, cmake_ops):
-        super(CMakeBuilder, self).__init__("cmake", prj_dir)
-        cmake_cmd = ["cmake", "-DCMAKE_BUILD_TYPE=Release", self._prj_dir] + self._parse_cmake_options(cmake_ops)
+    def __init__(self, prj_dir, target, runnable, cmake_ops, *args):
+        super(CMakeBuild, self).__init__("cmake", prj_dir, runnable)
+        if "run_cmake" in args:
+            cmake_cmd = ["cmake", "-DCMAKE_BUILD_TYPE=Release", self._prj_dir] + self._parse_cmake_options(cmake_ops)
+            call_with_cwd(cmake_cmd, self.build_dir())
+        else:
+            self._build_dir = "."
         make_cmd = ["make", target]
-        call_with_cwd(cmake_cmd, self._tmpdir.name)
-        call_with_cwd(make_cmd, self._tmpdir.name)
+        call_with_cwd(make_cmd, self.build_dir())
+
+    def run(self, args):
+        runnable = os.path.join(self.build_dir(), self.runnable())
+        call_args = [runnable] + args
+        return call_output(call_args, cwd=self.build_dir(), timeout=30)
 
     @staticmethod
     def _parse_cmake_options(flags):
@@ -97,33 +121,43 @@ class CMakeBuilder(Builder):
         return res
 
 
-class MonoBuilder(Builder):
+class MonoBuild(Build):
 
     def __init__(self, prj_dir, target, runnable, options):
-        super(MonoBuilder, self).__init__("mono", prj_dir)
+        super(MonoBuild, self).__init__("mono", prj_dir, runnable)
         compile_cmd = ["dmcs", os.path.join(self._prj_dir, runnable + ".cs")]
         call_with_cwd(compile_cmd, self.build_dir())
         dst = os.path.join(self.build_dir(), os.path.dirname(runnable))
         os.makedirs(dst)
         shutil.copy(os.path.join(prj_dir, runnable + ".exe"), dst)
 
+    def run(self, args):
+        runnable = os.path.join(self.build_dir(), self.runnable())
+        call_args = ["mono", runnable + ".exe"] + args
+        return call_output(call_args, cwd=self.build_dir(), timeout=30)
 
-class PythonBuilder(Builder):
+
+class PythonBuild(Build):
 
     def __init__(self, prj_dir, target, runnable, options):
-        super(PythonBuilder, self).__init__("python", prj_dir)
+        super(PythonBuild, self).__init__("python", prj_dir, runnable)
         dst = os.path.join(self.build_dir(), os.path.dirname(runnable))
         os.makedirs(dst)
         shutil.copy(os.path.join(prj_dir, runnable + ".py"), dst)
 
+    def run(self, args):
+        runnable = os.path.join(self.build_dir(), self.runnable())
+        call_args = ["python3", runnable + ".py"] + args
+        return call_output(call_args, cwd=self.build_dir(), timeout=30)
 
-def create_builder(builder_name, *args):
-    if builder_name == "cmake":
-        return CMakeBuilder(*args)
-    elif builder_name == "mono":
-        return MonoBuilder(*args)
-    elif builder_name == "python":
-        return PythonBuilder(*args)
+
+def build(builder, *args):
+    if builder == "cmake":
+        return CMakeBuild(*args)
+    elif builder == "mono":
+        return MonoBuild(*args)
+    elif builder == "python":
+        return PythonBuild(*args)
 
 
 class Scanner:
@@ -142,7 +176,7 @@ class Scanner:
                     action(match)
 
 
-class BoehmTestOutputParser:
+class Parser:
 
     def __init__(self):
 
@@ -159,10 +193,10 @@ class BoehmTestOutputParser:
             self._context["gc_count"] += [int(match.group("gc_count"))]
 
         token_spec = {
-            "FULL_TIME": {"cmd": parse_full_time, "re": "Completed in (?P<full_time>\d*) ms"},
-            "GC_TIME" : {"cmd": parse_gc_time, "re": "Time spent in gc (?P<gc_time>\d*) ms"},
-            "STW_TIME" : {"cmd": parse_stw_time, "re": "Average pause time (?P<stw_time>\d*) us"},
-            "GC_COUNT" : {"cmd": parse_gc_count, "re": "Completed (?P<gc_count>\d*) collections"}
+            "FULL_TIME" : {"cmd": parse_full_time, "re": "Completed in (?P<full_time>\d*) ms"},
+            "GC_TIME"   : {"cmd": parse_gc_time, "re": "Time spent in gc (?P<gc_time>\d*) ms"},
+            "STW_TIME"  : {"cmd": parse_stw_time, "re": "Average pause time (?P<stw_time>\d*) us"},
+            "GC_COUNT"  : {"cmd": parse_gc_count, "re": "Completed (?P<gc_count>\d*) collections"}
         }
 
         self._scanner = Scanner(token_spec)
@@ -172,21 +206,21 @@ class BoehmTestOutputParser:
         self._scanner.scan(test_output)
 
     def result(self):
-        stats = {}
-        stats[STATS_FULL_TIME_MEAN] = stat_mean(self._context["full_time"], ndigits=0)
-        stats[STATS_FULL_TIME_STD]  = stat_std(self._context["full_time"], ndigits=3)
-        stats[STATS_GC_TIME_MEAN]   = stat_mean(self._context["gc_time"], ndigits=0)
-        stats[STATS_GC_TIME_STD]    = stat_std(self._context["gc_time"], ndigits=3)
-        stats[STATS_STW_TIME_MEAN]  = stat_mean(self._context["stw_time"], ndigits=0)
-        stats[STATS_STW_TIME_STD]   = stat_std(self._context["stw_time"], ndigits=3)
-        stats[STATS_STW_TIME_MAX]   = stat_max(self._context["stw_time"], ndigits=0)
-        stats[STATS_GC_COUNT]       = stat_mean(self._context["gc_count"], default=0, ndigits=0)
-        return stats
+        return {
+            "STATS_FULL_TIME_MEAN"  : stat_mean(self._context["full_time"], ndigits=0),
+            "STATS_FULL_TIME_STD"   : stat_std(self._context["full_time"], ndigits=3),
+            "STATS_GC_TIME_MEAN"    : stat_mean(self._context["gc_time"], ndigits=0),
+            "STATS_GC_TIME_STD"     : stat_std(self._context["gc_time"], ndigits=3),
+            "STATS_STW_TIME_MEAN"   : stat_mean(self._context["stw_time"], ndigits=0),
+            "STATS_STW_TIME_STD"    : stat_std(self._context["stw_time"], ndigits=3),
+            "STATS_STW_TIME_MAX"    : stat_max(self._context["stw_time"], ndigits=0),
+            "STATS_GC_COUNT"        : stat_mean(self._context["gc_count"], default=0, ndigits=0)
+        }
 
 
 def create_parser(target):
     if target in ("boehm", "multisize_boehm", "parallel_merge_sort", "pyboehm", "csboehm"):
-        return BoehmTestOutputParser()
+        return Parser()
 
 
 class TexTableReporter:
@@ -206,6 +240,7 @@ class TexTableReporter:
         self._rows.append(self._match_columns(row, self._cols))
 
     def create_report(self):
+        import tabulate
         with open(self._outfn, "w") as outfile:
             outfile.write(tabulate.tabulate(self._rows, self._cols, tablefmt="latex"))
 
@@ -222,28 +257,21 @@ def create_reporter(reporter_name, outfn):
         return TexTableReporter(outfn)
 
 
-class NumpyDecoder(json.JSONEncoder):
-
-    def default(self, obj):
-        if isinstance(obj, np.integer):
-            return int(obj)
-        elif isinstance(obj, np.floating):
-            return float(obj)
-        elif isinstance(obj, np.ndarray):
-            return obj.tolist()
-        else:
-            return super(NumpyDecoder, self).default(obj)
-
-
 class RunChecker:
 
-    def __init__(self):
+    def __init__(self, maxn=None, assert_on_fail=False):
         self._runs = collections.defaultdict(lambda: collections.defaultdict(int))
+        self._maxn = maxn
+        self._assert_on_fail = assert_on_fail
 
     def check_run(self, target, run_name, rc):
         name = self._full_name(target, run_name)
         self._runs[name]["run count"] += 1
+        if self._maxn is not None and self._runs[name]["run count"] > self._maxn:
+            assert False, "Too many runs"
         if rc != 0:
+            if self._assert_on_fail:
+                assert False, "Run failed with error code %d" % rc
             self._runs[name]["failed count"] += 1
         return rc == 0
 
@@ -265,17 +293,6 @@ class RunChecker:
         return target + " " + run_name
 
 
-def run(builder, runnable, args):
-    runnable = os.path.join(builder.build_dir(), runnable)
-    if builder.name() == "mono":
-        call_args = ["mono", runnable + ".exe"] + args
-    elif builder.name() == "python":
-        call_args = ["python3", runnable + ".py"] + args
-    else:
-        call_args = [runnable] + args
-    return call_output(call_args, cwd=builder.build_dir(), timeout=30)
-
-
 class TestRunner:
 
     def __init__(self, prj_dir, target, builder, runnable, builds):
@@ -287,14 +304,15 @@ class TestRunner:
 
     def run(self, nruns, run_checker, reporters=[]):
 
-        for build in self._builds:
-            build_name = build.get("name")
-            cppflags   = build.get("compile_options")
+        for build_cfg in self._builds:
+            build_name = build_cfg.get("name")
+            cppflags   = build_cfg.get("compile_options")
+            options    = build_cfg.get("options", [])
 
             logging.info("Build {} with cppflags: {}".format(self._target, cppflags))
-            with create_builder(self._builder, self._prj_dir, self._target, self._runnable, cppflags) as builder:
 
-                run_ops = build.get("runtime_options", [{}])
+            with build(self._builder, self._prj_dir, self._target, self._runnable, cppflags, *options) as bld:
+                run_ops = build_cfg.get("runtime_options", [{}])
                 for run_op in run_ops:
                     run_name = build_name + " " + run_op.get("suffix", "")
                     args = run_op.get("args", "").split()
@@ -305,7 +323,7 @@ class TestRunner:
                     while n < nruns:
                         logging.info("Run {} with args: {}".format(self._runnable, args))
                         try:
-                            rc, output = run(builder, self._runnable, args)
+                            rc, output = bld.run(args)
                         except subprocess.TimeoutExpired:
                             logging.info("Interrupted (timeout expired)!")
                             run_checker.interrupted_run(self._target, run_name)
@@ -318,7 +336,7 @@ class TestRunner:
                             n += 1
 
                     parsed = parser.result()
-                    logging.debug("Parsed: \n {}".format(json.dumps(parsed, cls=NumpyDecoder)))
+                    logging.debug("Parsed: \n {}".format(json.dumps(parsed)))
 
                     logging.info("Add parsed output to reporter")
                     parsed["name"] = run_name
@@ -330,26 +348,82 @@ class TestRunner:
             reporter.create_report()
 
 PROJECT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../")
-CONFIG = "benchcfg.json"
 
 if __name__ == '__main__':
+
+    opts, args = getopt.getopt(sys.argv[1:], "", ["cfg=", "run-cmake"])
+    cli_options = []
+    for opt, arg in opts:
+        if opt == "--cfg":
+            config = arg
+        elif opt == "--run-cmake":
+            cli_options.append("run_cmake")
 
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
 
-    with open(CONFIG) as fd:
+    with open(config) as fd:
         cfg = json.load(fd)
 
-    run_checker = RunChecker()
+    nruns = cfg["nruns"]
+    failquick = cfg.get("failquick", False)
+    global_options = cfg.get("options", [])
 
-    for target in cfg["targets"]:
-        builds = target.get("builds", cfg["builds"])
-        runner = TestRunner(PROJECT_DIR, target["name"], target["builder"], target["runnable"], builds)
+    run_checker = RunChecker(maxn=3*nruns, assert_on_fail=failquick)
+
+    for target_cfg in cfg["targets"]:
+        target      = target_cfg["name"]
+        builder     = target_cfg["builder"]
+        runnable    = target_cfg["runnable"]
 
         reporters = []
         for reporter_ops in cfg.get("reporters", []):
-            reporters.append(create_reporter(reporter_ops["name"], target["name"] + reporter_ops["output"]))
+            reporters.append(create_reporter(reporter_ops["name"], target + reporter_ops["output"]))
 
-        runner.run(cfg["nruns"], run_checker, reporters)
+        builds = target_cfg.get("builds", cfg["builds"])
+        for build_cfg in builds:
+            build_name = build_cfg.get("name")
+            cppflags   = build_cfg.get("compile_options")
+            options    = build_cfg.get("options", [])
+            options   += global_options
+            options   += cli_options
+
+            logging.info("Build {} with cppflags: {}".format(target, cppflags))
+
+            with build(builder, PROJECT_DIR, target, runnable, cppflags, *options) as bld:
+                run_ops = build_cfg.get("runtime_options", [{}])
+                for run_op in run_ops:
+                    run_name = build_name + " " + run_op.get("suffix", "")
+                    args = run_op.get("args", "").split()
+
+                    parser = create_parser(target)
+
+                    n = 0
+                    while n < nruns:
+                        logging.info("Run {} with args: {}".format(runnable, args))
+                        try:
+                            rc, output = bld.run(args)
+                        except subprocess.TimeoutExpired:
+                            logging.info("Interrupted (timeout expired)!")
+                            run_checker.interrupted_run(target, run_name)
+                            continue
+                        logging.info("Return code: {}".format(rc))
+                        logging.debug("Output: \n {}".format(output))
+                        if run_checker.check_run(target, run_name, rc):
+                            logging.info("Parse output")
+                            parser.parse(output)
+                            n += 1
+
+                    parsed = parser.result()
+                    logging.debug("Parsed: \n {}".format(json.dumps(parsed)))
+
+                    logging.info("Add parsed output to reporter")
+                    parsed["name"] = run_name
+                    for reporter in reporters:
+                        reporter.add_stats(parsed)
+
+        logging.info("Produce reports")
+        for reporter in reporters:
+            reporter.create_report()
 
     print(run_checker.get_results())
